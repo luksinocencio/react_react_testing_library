ae6357a3cc7a39681ae9f817cbe437bc59ff2375
luksinocencio <luksinocencio@gmail.com>
2024-01-29 03:54:59 -0300

    *

--- project07/server/index.mjs
+++ project07/server/index.mjs
@@ -4,27 +4,27 @@ import cookieSession from 'cookie-session';
 import routes from './routes.mjs';
 
 const initServer = async () => {
-  const app = express();
+    const app = express();
 
-  app.use(express.json());
-  app.use(
-    cookieSession({
-      name: 'session',
-      keys: [process.env.COOKIE_SECRET],
-      httpOnly: true,
-      sameSite: 'strict',
-    })
-  );
-  app.use((req, res, next) => {
-    console.log(req.method.toUpperCase(), '-', req.url);
-    next();
-  });
-  app.use(routes);
+    app.use(express.json());
+    app.use(
+        cookieSession({
+            name: 'session',
+            keys: [process.env.COOKIE_SECRET],
+            httpOnly: true,
+            sameSite: 'strict',
+        })
+    );
+    app.use((req, res, next) => {
+        // console.log(req.method.toUpperCase(), '-', req.url);
+        next();
+    });
+    app.use(routes);
 
-  const PORT = 8000;
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+    const PORT = 8000;
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`);
+    });
 };
 
 initServer();
